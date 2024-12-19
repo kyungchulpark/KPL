c25a85af9f2ebd7dccbc6edd945efa8a7dccc3c2
BOOK-G0U23C7LVT\pkc09 <parkkc@akiaka.co.kr>
2024-12-19 18:06:09 +0900

    테마 적용

--- src/main/java/com/kpl/kpl/controller/HomeController.java
+++ src/main/java/com/kpl/kpl/controller/HomeController.java
@@ -8,6 +8,13 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index"; // templates 폴더에 있는 index.html 반환
+        return "esports-home"; // templates 폴더에 있는 index.html 반환
     }
+
+    @GetMapping("/esports-post")
+    public String showPostPage() {
+        return "esports-post"; // 타임리프 템플릿 이름 (확장자 .html 제외)
+    }
+
+
 }
